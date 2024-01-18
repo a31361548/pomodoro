eaad6654f7033c8932f08fd5fd617ca7674e335a
a31361548 <a31361548@gmail.com>
2024-01-18 17:21:57 +0800

    renew

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,6 +20,125 @@
         ></v-btn>
       </v-col>
     </v-row>
+
+    <!-- 卡片插槽 -->
+    <v-dialog width="500">
+  <template v-slot:activator="{ props }">
+    <v-btn v-bind="props" text="有種來啊!!!"> </v-btn>
+  </template>
+
+  <template v-slot:default="{ isActive }">
+    <v-card title="對不起，我錯了">
+      <v-card-text>
+       <v-icon icon="mdi-close"></v-icon>
+       <v-icon icon="mdi-close"></v-icon>
+       <br>
+       <span>　O</span>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          text="Close Dialog"
+          @click="isActive.value = false"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+    </v-dialog>
+
+    <v-card
+    class="ml-auto"
+    max-width="368"
+  >
+    <v-card-item title="Florida">
+      <template v-slot:subtitle>
+        <v-icon
+          icon="mdi-alert"
+          size="18"
+          color="error"
+          class="me-1 pb-1"
+        ></v-icon>
+
+        Extreme Weather Alert
+      </template>
+    </v-card-item>
+
+    <v-card-text class="py-0">
+      <v-row align="center" no-gutters>
+        <v-col
+          class="text-h2"
+          cols="6"
+        >
+          64&deg;F
+        </v-col>
+
+        <v-col cols="6" class="text-right">
+          <v-icon
+            color="error"
+            icon="mdi-weather-hurricane"
+            size="88"
+          ></v-icon>
+        </v-col>
+      </v-row>
+    </v-card-text>
+
+    <div class="d-flex py-3 justify-space-between">
+      <v-list-item
+        density="compact"
+        prepend-icon="mdi-weather-windy"
+      >
+        <v-list-item-subtitle>123 km/h</v-list-item-subtitle>
+      </v-list-item>
+
+      <v-list-item
+        density="compact"
+        prepend-icon="mdi-weather-pouring"
+      >
+        <v-list-item-subtitle>48%</v-list-item-subtitle>
+      </v-list-item>
+    </div>
+
+    <v-expand-transition>
+      <div v-if="expand">
+        <div class="py-2">
+          <v-slider
+            v-model="time"
+            :max="6"
+            :step="1"
+            :ticks="labels"
+            class="mx-4"
+            color="primary"
+            density="compact"
+            hide-details
+            show-ticks="always"
+            thumb-size="10"
+          ></v-slider>
+        </div>
+
+        <v-list class="bg-transparent">
+          <v-list-item
+            v-for="item in forecast"
+            :key="item.day"
+            :title="item.day"
+            :append-icon="item.icon"
+            :subtitle="item.temp"
+          >
+          </v-list-item>
+        </v-list>
+      </div>
+    </v-expand-transition>
+
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-btn @click="expand = !expand">
+        {{ !expand ? 'Full Report' : 'Hide Report' }}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
   </v-container>
 </template>
 
@@ -109,4 +228,15 @@ const currentTime = computed(() => {
   const s = (timeleft.value % 60).toString().padStart(2, '0')
   return m + ':' + s
 })
+
+// test weather
+const labels = { 0: 'SU', 1: 'MO', 2: 'TU', 3: 'WED', 4: 'TH', 5: 'FR', 6: 'SA' }
+const forecast = [
+  { day: 'Tuesday', icon: 'mdi-white-balance-sunny', temp: '24\u00B0/12\u00B0' },
+  { day: 'Wednesday', icon: 'mdi-white-balance-sunny', temp: '22\u00B0/14\u00B0' },
+  { day: 'Thursday', icon: 'mdi-cloud', temp: '25\u00B0/15\u00B0' }
+]
+
+const expand = ref(false)
+const time = ref(0)
 </script>

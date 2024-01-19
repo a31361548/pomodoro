92a518b37104572784fb29d5203c7758dfbac259
a31361548 <a31361548@gmail.com>
2024-01-19 11:09:24 +0800

    renew

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <v-app>
-    <v-app-bar>
+    <v-app-bar image="../images/bg1.jpg">
       <v-container class="d-flex align-center">
         <v-app-bar-title>番茄鐘</v-app-bar-title>
         <v-btn prepend-icon="mdi-home" to="/">首頁</v-btn>
         <v-btn prepend-icon="mdi-list-status" to="/list">事項</v-btn>
         <v-btn prepend-icon="mdi-cog" to="/settings">設定</v-btn>
+         <!-- 更改背景色 -->
+         <v-btn @click="toggleTheme" class="switchButton">switch</v-btn>
       </v-container>
     </v-app-bar>
     <v-main>
@@ -28,4 +30,18 @@
 </template>
 
 <script setup>
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+// 背景切換
+function toggleTheme () {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
 </script>
+
+<style>
+.switchButton:hover{
+ color: rgb(23, 183, 211);
+}
+</style>
